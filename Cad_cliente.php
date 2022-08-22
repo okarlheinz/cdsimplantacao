54ce9dfbada2ead24e28262a83c72d2b67294196
Karl Heinz <okarlheinz@outlook.com>
2022-08-22 00:25:32 -0300

    Atualizacoes na tela de cadastro de cliente

--- Cad_cliente.php
+++ Cad_cliente.php
@@ -27,34 +27,32 @@
 		<h1>Cadastro de Cliente</h1>
 	</div>
 	<div class="container">
-
-		<form method='post' action="Cad_cliente.php">
+		<form class='cadastro' method='post' action="Cad_cliente.php">
 			<div>
 				<label for="fname">Fantasia:</label><br>
 				<input type="text" id="fantasia" name="fantasia" ><br><br>
-			</div>
-			<div>
 				<label for="fname">CDSWEB Login:</label><br>
 				<input type="text" id="cdsweblogin" name="cdsweblogin" ><br><br>
 			</div>
 			<div>
 				<label for="lname">CDSWEB Senha:</label><br>
 				<input type="text" id="cdswebsenha" name="cdswebsenha" ><br><br>
-			</div>
-			<div>	
 				<label for="lname">ODBC Login:</label><br>
 				<input type="text" id="odbclogin" name="odbclogin" ><br><br>
 			</div>
-			<div>
-				<label for="lname">ODBC Senha:</label><br>
+			<div>				
+				<label for="lname">ODBC Senha:</label><br> 
 				<input type="text" id="odbcsenha" name="odbcsenha" ><br><br>
-			</div>
-			<div>
 				<label for="lname">Servidor:</label><br>
-				<input type="text" id="servidor" name="servidor" ><br><br>
+				<select name="servidor" id="servidor">
+					<option value="63.143.45.98,4503">63.143.45.98,4503</option>
+					<option value="74.63.213.142,4502">74.63.213.142,4502</option>
+					<option value="138.128.173.162,4500">138.128.173.162,4500</option>
+				</select>
+				<!-- <input type="text" id="servidor" name="servidor" ><br><br> -->
 			</div>
-			<div>
-				<input type="submit" value="Submit">
+			<div class='submit'>
+				<input type="submit" value="Salvar">
 			</div>
 		</form> 
 	</div>
