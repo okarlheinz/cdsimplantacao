5620d9cc375838008a9e385386c3a06920fa2461
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-03-23 14:30:49 -0300

    atualizacoes na tela de downloads

--- Downloads.php
+++ Downloads.php
@@ -9,12 +9,28 @@
 				<div class="container">
 
 					<h1>Downloads</h1>	
-						<button class="btn-download"><a href="downloads/Gerador_de_cpf.rar" target="_blank" rel="noopener noreferrer">Gerador de CPF</a></button>
-						<button class="btn-download"><a href="downloads/AnyDesk.exe" target="_blank" rel="noopener noreferrer">Any Desk</a></button>
-						<button class="btn-download"><a href="downloads/AcessoRemoto.exe" target="_blank" rel="noopener noreferrer">Acesso Remoto</a></button>
-						<button class="btn-download"><a href="http://198.49.71.152:800/cdsnfe/" target="_blank" rel="noopener noreferrer">Instalador NFC-E</a></button>                 
-						<button class="btn-download"><a href="downloads/CDS_Baixaexe.rar" target="_blank" rel="noopener noreferrer">Baixa Copia</a></button>                 
-						<button class="btn-download"><a href="https://drive.google.com/file/d/1_XP9nua0TMGg8EXN1mNUBayFwduW0xfE/view?usp=sharing" target="_blank" rel="noopener noreferrer">Instalador SQL 2008</a></button>                 
+						<button class="btn-download">
+							<a href="https://www.4devs.com.br/gerador_de_cpf" target="_blank" rel="noopener noreferrer">Gerador de CPF</a></button>
+						<button class="btn-download">
+							<a href="https://anydesk.com/pt/downloads/thank-you?dv=win_exe" target="_blank" rel="noopener noreferrer">Any Desk</a></button>
+						<button class="btn-download">
+							<a href="downloads/AcessoRemoto.exe" target="_blank" rel="noopener noreferrer">Acesso Remoto</a></button>
+						<button class="btn-download">
+							<a href="http://198.49.71.152:800/cdsnfe/InstaladorNFe4.0CDS.exe" rel="noopener noreferrer">Instalador NFC-e</a></button>                 
+						<button class="btn-download">
+							<a href="http://198.49.71.152:800/cdsnfe/CDSNFE.rar" rel="noopener noreferrer">Executavel NFC-e</a></button>                 
+						<button class="btn-download">
+							<a href="http://198.49.71.152:800/cdsnfe/CDS_Comercial_NFe.rar" rel="noopener noreferrer">Executavel Nota Avulsa</a></button>                 
+						<button class="btn-download">
+							<a href="http://198.49.71.152:800/cdsnfe/AtualizadorNFe.exe" rel="noopener noreferrer">Atualizador NF-e</a></button>                 
+						<button class="btn-download">
+							<a href="http://198.49.71.152:800/cdsnfe/setup_nfe_dependencies.rar" rel="noopener noreferrer">Dependencias NF-e</a></button>                 
+						<button class="btn-download">
+							<a href="downloads/CDS_Baixaexe.rar" target="_blank" rel="noopener noreferrer">Baixa Copia</a></button>                 
+						<button class="btn-download">
+							<a href="https://drive.google.com/file/d/1_XP9nua0TMGg8EXN1mNUBayFwduW0xfE/view?usp=sharing" target="_blank" rel="noopener noreferrer">Instalador SQL 2008</a></button>                 
+						<button class="btn-download">
+							<a href="https://chat.openai.com/" target="_blank" rel="noopener noreferrer">ChatGPT</a></button>                 
 
 				</div>
 			</div>
