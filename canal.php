ee41444b2b90189f5c7e20ed892713ef56552eb5
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-03-23 13:47:37 -0300

    NOVOS VIDEOS INSERIDOS

--- canal.php
+++ canal.php
@@ -17,6 +17,18 @@
             <h3>Consulta de credenciamento e CSC de um certificado digital</h3>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/0RYzdsvWLog?si=8S6x46X2uTAL-g0y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
+        <div class="cards">
+            <h3>Operações básicas no migrate para suporte e implantação</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/WK6orHLQOCE?si=kZ45HtOt_XctSKpu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="cards">
+            <h3>DESENVOLVIMENTO DE ETIQUETA PPLA/PPLB (ARGOX/ELGIN) PARTE 1</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Az-t5H_Ug8g?si=4-RrfWMiEa4Sysly" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+        <div class="cards">
+            <h3>DESENVOLVIMENTO DE ETIQUETA PPLA/PPLB (ARGOX/ELGIN) PARTE 2</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/Vp4gwTUuXQQ?si=n7KrWVD2KLaMgozJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
     </div>
 
 </body>
